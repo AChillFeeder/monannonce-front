35806cb9a6b69ffacad87bf8fb77bcbb014b917e
A chill feeder <mohamedredaloukhnati@yahoo.com>
2025-01-13 12:42:10 +0100

    depot initial

---
+++ monannonce-front/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+// 4 écrans avec lien entre eux
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen</Text>
+    </View>
+  );
+}
